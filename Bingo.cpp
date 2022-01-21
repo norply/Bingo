8e2bb36b72ad8ba3e2b6c0c06fc141e75b80c531
norply <75418405+norply@users.noreply.github.com>
2022-01-21 19:36:56 +0500

    Начало
    Добавляем библиотеки для ввода/вывода, русского языка, определения нажатия клавиши, использования векторов, генерации случайных чисел и ведения log файла. Подключаем русский язык.

---
+++ Bingo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>// Для ввода-вывода
+#include <Windows.h>// Для подключения русского языка
+#include <conio.h>// Для определения нажатия клавиши
+#include <vector>// Для использования векторов
+#include <random>// Для генерации случайных чисел
+#include <fstream>// Для ведения log файла
+using namespace std;
+int main()
+{
+SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+    setlocale(LC_CTYPE, "rus");
+    
+ }
