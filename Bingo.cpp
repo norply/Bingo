c55e5dca54342190778c39f290bfc88b441905c3
norply <75418405+norply@users.noreply.github.com>
2022-01-21 20:09:22 +0500

    Логирование
    Добавляем ведение log файла, в котором будет отображаться ввод пользователя и полученные бочонки.

--- Bingo.cpp
+++ Bingo.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 int main()
 {
+    ofstream file("Bingo.log", ios_base::app);
+    file << endl << endl << "Начало работы программы";
 SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     setlocale(LC_ALL, "Russian");
@@ -15,6 +17,7 @@ SetConsoleCP(1251);
     std::mt19937 mersenne(rd());
     float n;// Число бочонков
     int rand;// Случайное число от 0 до n-1;
+    file << endl << "Ввод числа бочонков";
      cout << "Введите число бочонков: ";
     for (;;)// Проверка ввода числа бочонков
     {
@@ -22,21 +25,25 @@ SetConsoleCP(1251);
         if (!cin)
         {
             cout << "Введите число: ";
+            file << endl << "Введено не число";
         }
         else
         {
             if (int(n) != n)
             {
                 cout << "Введите целое число: ";
+                file << endl << "Введено не целое число: " << n;
             }
              else
             {
                 if (n <= 0)
                 {
                     cout << "Введите положительное число: ";
+                    file << endl << "Введено не положительное число: " << n;
                 }
                  else
                 {
+                     file <<endl<< "Число бочонков: " << n;
                      break;
                  }
                  }
@@ -63,7 +70,9 @@ SetConsoleCP(1251);
         
         
         cout << endl<< "Выпал бочонок номер: "<< barrels[rand];
+         file << endl << "Выпал бочонок номер: " << barrels[rand];
           barrels.erase(barrels.begin() + rand);
         n = n - 1;
     }
+    file << endl << "Завершение работы программы";
  }
